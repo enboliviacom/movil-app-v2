431ec18bcacc622a5a841b7308fba6dca5d60a55
Juan Carlos Choque Quispe <juan@enbolivia.com>
2013-04-03 15:34:55 -0400

    Actualizando cambios para aplicación inicial

--- src/com/enbolivia/cotizadorv2/MainActivity.java
+++ src/com/enbolivia/cotizadorv2/MainActivity.java
@@ -1,22 +1,16 @@
 package com.enbolivia.cotizadorv2;
 
+import org.apache.cordova.Config;
+import org.apache.cordova.DroidGap;
+
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends DroidGap {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		//setContentView(R.layout.activity_main);
+		super.loadUrl( Config.getStartUrl() );
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
 }
