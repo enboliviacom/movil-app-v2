593da5ebfa4d520dbb618d9a06107154e628b442
Juan Carlos Choque Quispe <juan@enbolivia.com>
2013-04-05 15:31:29 -0400

    Juanc, agregando funciones para creación de base de datos

--- assets/www/js/functions.js
+++ assets/www/js/functions.js
@@ -1,7 +1,16 @@
 var deviceIsOnLine = false;
 var db;
+var dbName = "cotizador";
+var dbDescription = "Cotizador";
+var dbVersion = "1.0";
+var dbSizeMB = 7;
+var tablePackages = "paquetes";
+var tablePackagesExists = false;
 
 function onLoad() {
+	/*$.mobile.allowCrossDomainPages = true;
+    $.support.cors = true;*/
+    
 	document.addEventListener( "deviceready", onDeviceReady, false );
 	document.addEventListener( "online", onOnline, false );
 	document.addEventListener( "offline", onOffline, false );
@@ -24,19 +33,89 @@ function onOffline() {
 	deviceIsOnLine = false;
 }
 
+/**
+ * Function to connect whit SQLite database
+ */
 function initializeDB() {
-	db = window.openDatabase( "cotizador", "1.0", "Cotizador", 7000000 );
+	db = window.openDatabase( dbName, dbDescription, dbVersion, dbSizeMB * 1024 * 1024 );
 }
 
 function checkCotizadorData() {
 	$.ajax({
-		url: 'http://localhost.enbolivia/dataCotizador.php',
+		url: 'http://www.cuencasbolivia.org/gapi/dataCotizador.json',
 		dataType: 'json',
 		success: parseAndSaveData,
-		error: function ( data ) { console.log( "There was an error when the app tried to get new data." ) }
+		error: function(request, status, error) {
+		      	console.log("Error status " + status);
+		      	console.log("Error request status text: " + request.statusText);
+		      	console.log("Error request status: " + request.status);
+		      	console.log("Error request response text: " + request.responseText);
+		      	console.log("Error response header: " + request.getAllResponseHeaders());
+		    	}
 	});
 }
 
+/**
+ * Function to parse JSON format and save it into a database table
+ * @param data, string-json
+ */
 function parseAndSaveData( data ) {
-	console.log( data );
+	//--console.log( data );
+	
+	db.transaction( checkCurrentTables, errorCB, successCB );
+	
+	if( !tablePackagesExists ) {
+		db.transaction( createPackageTable, errorCB, successCB );
+	}
+	$("#loading").hide();
+}
+
+/**
+ * Function to create packages table and add to it and index 
+ * @param tx. SQLTransaction object
+ */
+function createPackageTable( tx ) {
+	var query = "CREATE TABLE IF NOT EXISTS " + tablePackages + " (" +
+			"id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+			"title TEXT NOT NULL, description TEXT NULL, " +
+			"modules TEXT NOT NULL, image TEXT NULL, " +
+			"optional_modules TEXT NULL, fixed_modules TEXT NULL)";
+	
+	tx.executeSql( query, [], function ( tx, result ) {
+		console.log( "Table " + tablePackages + " created successfully" );
+	}, errorCB );
+	
+	query = "CREATE INDEX IF NOT EXISTS datatypes ON " + tablePackages + " (modules, optional_modules, fixed_modules)";
+	
+	tx.executeSql( query, [], function ( tx, result ) {
+		console.log( "Index in " + tablePackages + " created successfully" );
+	}, errorCB );
+}
+
+/**
+ * Function to check if there are tables in the SQLite database
+ * @param tx, SQLTransaction object
+ */
+function checkCurrentTables( tx ) {
+	var query = "SELECT name FROM sqlite_master WHERE type='table'";
+	
+	tx.executeSql( query, [], function( tx, result ) {
+		var len = result.rows.length;
+		
+		for( var i = 0; i < len; i++ ) {
+			/* check if database table is already created */
+			if( result.rows.item(i).name == tablePackages ) {
+				tablePackagesExists = true;
+				break;
+			}
+		}
+	}, errorCB );
+}
+
+function errorCB( err ) {
+	console.log( "There was an error procesing the sql query." );
+}
+
+function successCB() {
+	console.log( "Transaction executed successfully" );
 }
